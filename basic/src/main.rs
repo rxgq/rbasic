ecf91a549b9b39bbadb8658de1abcec49ff3daf5
Ashton Dunderdale <ashtondunderdale@gmail.com>
2024-08-23 16:35:48 +0100

    change

--- basic/src/main.rs
+++ basic/src/main.rs
@@ -1,3 +1,17 @@
-fn main() {
+use std::fs;
 
+use lexer::{Lexer, Token};
+
+mod lexer;
+
+fn main() {
+    let source = fs::read_to_string("example/code.txt").expect("could not open file");
+    let mut lexer = Lexer::new(source);
+
+    let tokens: Vec<Token> = lexer.tokenize();
+    for token in tokens {
+        println!("'{}' = {:?}", token.lexeme, token.token_type);
+    }
+
+    
 }
