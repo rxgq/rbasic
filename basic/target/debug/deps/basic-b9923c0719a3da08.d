e8598128028e5ba472bf0beda3190248be6c1265
Ashton Dunderdale <ashtondunderdale@gmail.com>
2024-08-23 14:40:49 +0100

    init

---
+++ basic/target/debug/deps/basic-b9923c0719a3da08.d
@@ -0,0 +1,5 @@
+C:\Users\adunderdale\Projects\personal\basic\basic\target\debug\deps\libbasic-b9923c0719a3da08.rmeta: src/main.rs
+
+C:\Users\adunderdale\Projects\personal\basic\basic\target\debug\deps\basic-b9923c0719a3da08.d: src/main.rs
+
+src/main.rs:
---
+++ basic/target/debug/deps/basic-e32c6549a28ee665.d
@@ -0,0 +1,5 @@
+C:\Users\adunderdale\Projects\personal\basic\basic\target\debug\deps\libbasic-e32c6549a28ee665.rmeta: src/main.rs
+
+C:\Users\adunderdale\Projects\personal\basic\basic\target\debug\deps\basic-e32c6549a28ee665.d: src/main.rs
+
+src/main.rs:
