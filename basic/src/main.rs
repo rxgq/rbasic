80c2ffc518207aa5849f4f866bce7c3907693a0c
rxgq <ashtondunderdale@gmail.com>
2024-08-24 10:55:51 +0100

    lexer

--- basic/src/main.rs
+++ basic/src/main.rs
@@ -1,17 +1,21 @@
-use std::fs;
-
-use lexer::{Lexer, Token};
+use lexer::Lexer;
+use parser::Parser;
 
 mod lexer;
+mod parser;
 
 fn main() {
-    let source = fs::read_to_string("example/code.txt").expect("could not open file");
-    let mut lexer = Lexer::new(source);
-
-    let tokens: Vec<Token> = lexer.tokenize();
-    for token in tokens {
-        println!("'{}' = {:?}", token.lexeme, token.token_type);
-    }
-
+    let source: String = std::fs::read_to_string("example/code.txt").expect("error reading file");
     
+    let mut lexer = Lexer::new(source);
+    let tokens = lexer.tokenize();
+    // for token in &tokens {
+    //     println!("{:?}", token);
+    // }
+
+    let mut parser = Parser::new(&tokens);
+    let exprs = parser.parse();
+    for expr in exprs {
+        println!("{:?}", expr)
+    }
 }
--- basic/target/debug/deps/basic-b9923c0719a3da08.d
+++ basic/target/debug/deps/basic-b9923c0719a3da08.d
@@ -1,6 +1,6 @@
-C:\Users\adunderdale\Projects\personal\basic\basic\target\debug\deps\libbasic-b9923c0719a3da08.rmeta: src/main.rs src\lexer.rs
+C:\Users\rxgqq\projects\basic\basic\target\debug\deps\libbasic-b9923c0719a3da08.rmeta: src/main.rs src\lexer.rs
 
-C:\Users\adunderdale\Projects\personal\basic\basic\target\debug\deps\basic-b9923c0719a3da08.d: src/main.rs src\lexer.rs
+C:\Users\rxgqq\projects\basic\basic\target\debug\deps\basic-b9923c0719a3da08.d: src/main.rs src\lexer.rs
 
 src/main.rs:
 src\lexer.rs:
